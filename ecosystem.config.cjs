8a48181efe43d96bc3e7e96a9bff24d83939be76
Mohamed Helmy <nMahmoud@systemtech.loc>
2026-08-05 08:30:33 +0300

    Run PM2 app in fork mode

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -8,6 +8,7 @@ module.exports = {
       script: "server.js",
       interpreter: "node",
       instances: 1,
+      exec_mode: "fork",
       autorestart: true,
       watch: false,
       max_restarts: 10,
